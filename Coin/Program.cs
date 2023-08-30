cdacf777fe23b60ac2962471640a3a79579205e6
Tyler Adams <skipmarcel@gmail.com>
2023-08-29 20:49:15 -0700

    Add ui

--- Coin/Program.cs
+++ Coin/Program.cs
@@ -10,7 +10,31 @@ namespace Coin
  {
   static void Main()
   {
-    
+   Console.WriteLine("Welcome to Coin Calculator!");
+   Console.WriteLine("Please enter a ¢ amount");
+
+  // int centAmount = Console.ReadLine();
+  // int changeTotal = centAmount.CoinCombinations();
+
+
+  string centAmount = Console.ReadLine();
+  int centInt = int.Parse(centAmount);
+  var changeTotal = CoinCalculator.CoinCombinations(centInt);
+
+  foreach (var value in changeTotal)
+  {
+    Console.WriteLine($"Your change is: ${value}.");
+  }
+  // Console.WriteLine($"Your change is: ${changeTotal}.");
+
   }
  }
 }
+
+
+
+
+
+// string str = "123";
+// int parsedValue = int.Parse(str);
+
